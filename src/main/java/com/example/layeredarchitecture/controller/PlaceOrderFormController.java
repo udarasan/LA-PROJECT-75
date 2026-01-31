8a82f2efe8afe21b3bfe552563ca02b609700a70
Udara San <udarassanjeewa@gmail.com>
2026-01-31 11:23:21 +0530

    generate new Order ID

--- src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
@@ -210,8 +210,8 @@ public class PlaceOrderFormController {
 //            Statement stm = connection.createStatement();
 //            ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
             OrderDAOImpl orderDAO = new OrderDAOImpl();
-            ResultSet rst = orderDAO.generateNewOrderId();
-            return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
+            return orderDAO.generateNewOrderId();
+
         } catch (SQLException e) {
             new Alert(Alert.AlertType.ERROR, "Failed to generate a new order id").show();
         } catch (ClassNotFoundException e) {
--- src/main/java/com/example/layeredarchitecture/dao/OrderDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/OrderDAOImpl.java
@@ -9,10 +9,12 @@ import java.sql.Statement;
 
 public class OrderDAOImpl {
 
-    public ResultSet generateNewOrderId() throws SQLException, ClassNotFoundException {
+    public String generateNewOrderId() throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         Statement stm = connection.createStatement();
         ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
-        return rst;
+        return rst.next() ? String.format("OID-%03d",
+                (Integer.parseInt(rst.getString("oid")
+                        .replace("OID-", "")) + 1)) : "OID-001";
     }
 }
