83a7f73bbf105e552573fe275adb19c202b4040d
Udara San <udarassanjeewa@gmail.com>
2026-01-31 13:36:49 +0530

    exitsOrder method

--- src/main/java/com/example/layeredarchitecture/dao/OrderDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/OrderDAOImpl.java
@@ -2,10 +2,7 @@ package com.example.layeredarchitecture.dao;
 
 import com.example.layeredarchitecture.db.DBConnection;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class OrderDAOImpl {
 
@@ -16,5 +13,13 @@ public class OrderDAOImpl {
         return rst.next() ? String.format("OID-%03d",
                 (Integer.parseInt(rst.getString("oid")
                         .replace("OID-", "")) + 1)) : "OID-001";
+    }
+    public boolean exitsOrder(String orderId) throws SQLException,
+            ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement stm = connection
+                .prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
+        stm.setString(1, orderId);
+        return stm.executeQuery().next();
     }
 }
