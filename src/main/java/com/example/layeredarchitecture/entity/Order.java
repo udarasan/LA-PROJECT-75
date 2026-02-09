4caefce660d40968bfb30365a102110a9407d9b7
Udara San <udarassanjeewa@gmail.com>
2026-02-09 11:21:04 +0530

    entities

---
+++ src/main/java/com/example/layeredarchitecture/entity/Order.java
@@ -0,0 +1,59 @@
+package com.example.layeredarchitecture.entity;
+
+import java.time.LocalDate;
+
+public class Order {
+    private String orderId;
+    private LocalDate orderDate;
+    private String customerId;
+    private String customerName;
+    private String orderTotal;
+
+    public  Order(String orderId,LocalDate orderDate,String customerId,String customerName,String orderTotal) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.customerId = customerId;
+        this.customerName = customerName;
+    }
+    public Order() {}
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void setOrderTotal(String orderTotal) {
+        this.orderTotal = orderTotal;
+    }
+}
